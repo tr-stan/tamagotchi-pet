50b9b258233177aaee824ff82201d702f114ac3a
Tristan Bennett <betriistan@gmail.com>
2019-01-04 11:43:10 -0600

    added die function and improved increment functions, added click event listeners to app.js

--- app.js
+++ app.js
@@ -12,13 +12,18 @@ class Tamagotchi {
 // create tamagotchi object
 let myTamagotchi = new Tamagotchi(1, 1, 1, 0);
 
+//create boolean for tamagotchi is alive or dead
+$("#restart").hide();
+let alive = true;
 //create functions to increment stats
 
 function incrementHunger() {
     let timeoutID = window.setTimeout(function() {
         myTamagotchi.hunger += 1;
         $("#hunger").replaceWith(`<p id='hunger'>Hunger: ${myTamagotchi.hunger}</p>`);
-        incrementHunger();
+        if (alive) {
+            incrementHunger();
+        }
     }, 15000)
 
 };
@@ -27,7 +32,9 @@ function incrementSleepiness() {
     let timeoutID = window.setTimeout(function() {
         myTamagotchi.sleepiness += 1;
         $("#sleepiness").replaceWith(`<p id='sleepiness'>Sleepiness: ${myTamagotchi.sleepiness}</p>`);
-        incrementSleepiness();
+        if (alive) {
+            incrementSleepiness();
+        }
     }, 20000)
 
 };
@@ -36,29 +43,15 @@ function incrementBoredom() {
     let timeoutID = window.setTimeout(function() {
         myTamagotchi.boredom += 2;
         $("#boredom").replaceWith(`<p id='boredom'>Boredom: ${myTamagotchi.boredom}</p>`);
-        incrementBoredom();
-    }, 20000)
+        if (alive) {
+            incrementBoredom();
+        }
+    }, 16000)
 
 };
 
-function hatch() {
-    let timeoutID = window.setTimeout(function() {
-        myTamagotchi.age += 1;
-        $("#age").replaceWith(`<p id='age'>Age: ${myTamagotchi.age}</p>`);
-        birth();
-    }, 10000)
-}
-
-function incrementAge() {
-    let timeoutID = window.setTimeout(function() {
-        myTamagotchi.age += 1;
-        $("#age").replaceWith(`<p id='age'>Age: ${myTamagotchi.age}</p>`);
-        incrementAge();
-    }, 5000)
-
-}
-
-function birth() {
+// create functions to hatch the tamagotchi after x second, increment age after y seconds
+function grow() {
     if (myTamagotchi.age < 1) {
         hatch();
     } else {
@@ -67,7 +60,103 @@ function birth() {
     }
 }
 
+function hatch() {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.age += 1;
+        $("#age").replaceWith(`<p id='age'>Age: ${myTamagotchi.age}</p>`);
+        grow();
+    }, 15000)
+}
+
+function incrementAge() {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.age += 1;
+        $("#age").replaceWith(`<p id='age'>Age: ${myTamagotchi.age}</p>`);
+        if (alive) {
+            incrementAge();
+        }
+    }, 20000)
+
+}
+
+
+//create functions to decrease stats
+function feed() {
+    if (myTamagotchi.hunger >= 2) {
+        myTamagotchi.hunger -= 2;
+        $("#hunger").replaceWith(`<p id='hunger'>Hunger: ${myTamagotchi.hunger}</p>`);
+    } else {
+        myTamagotchi.hunger = 0;
+        $("#hunger").replaceWith(`<p id='hunger'>Hunger: ${myTamagotchi.hunger}</p>`);
+    }
+}
+
+function play() {
+    if (myTamagotchi.boredom >= 3) {
+        myTamagotchi.boredom -= 3;
+        $("#boredom").replaceWith(`<p id='boredom'>Boredom: ${myTamagotchi.boredom}</p>`);
+    } else {
+        myTamagotchi.boredom = 0;
+        $("#boredom").replaceWith(`<p id='boredom'>Boredom: ${myTamagotchi.boredom}</p>`);
+    }
+}
+
+function bedtime(time) {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.sleepiness -= 1;
+        $("#sleepiness").replaceWith(`<p id='sleepiness'>Sleepiness: ${myTamagotchi.sleepiness}</p>`);
+    }, time)
+}
+
+// create functions to die and to restart game
+function restart() {
+    let alive = true;
+    $("#restart").hide();
+    $("img:eq(1)").replaceWith("<img src='tamagotchi-bunny-2.png'>");
+    myTamagotchi.sleepiness = 0;
+    $("#sleepiness").replaceWith(`<p id='sleepiness'>Sleepiness: ${myTamagotchi.sleepiness}</p>`);
+    $("#sleep").show()
+    myTamagotchi.boredom += 2;
+    $("#boredom").replaceWith(`<p id='boredom'>Boredom: ${myTamagotchi.boredom}</p>`);
+    $("#play").show()
+    myTamagotchi.hunger = 0;
+    $("#hunger").replaceWith(`<p id='hunger'>Hunger: ${myTamagotchi.hunger}</p>`);
+    $("#feed").show()
+}
+
+function die() {
+    alive = false;
+    $("img:eq(1)").replaceWith("<img src='tamagotchi-dead.png'>");
+    $("#sleep").hide();
+    $("#play").hide();
+    $("#feed").hide();
+}
+
+// increment(myTamagotchi, hunger, 3000);
 incrementHunger();
 incrementSleepiness();
 incrementBoredom();
-birth();
+grow();
+
+//set up click event listeners
+$("#sleep").click(function() {
+    bedtime(5000);
+});
+
+$("#play").click(function() {
+    play();
+})
+
+$("#feed").click(function() {
+    feed();
+})
+
+$("#restart").click(function() {
+    restart();
+})
+
+
+if (myTamagotchi.boredom > 9 || myTamagotchi.hunger > 9 || myTamagotchi.sleepiness > 9) {
+    die();
+    $("#restart").show()
+}
