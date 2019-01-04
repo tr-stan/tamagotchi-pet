bb09f0698137cd73b7831a32cd8f205fdc7a165d
Tristan Bennett <betriistan@gmail.com>
2019-01-04 11:42:29 -0600

    added functions to incrememnt stats over time, function to change tamagotchi form when its age turns to 1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// create class for creating tamagotchi objects
 class Tamagotchi {
     constructor(hunger, sleepiness, boredom, age) {
         this.hunger = hunger;
@@ -7,9 +8,66 @@ class Tamagotchi {
     }
 
 }
-let myTamagotchi = new Tamagotchi(1, 0, 0, 0);
 
-$("#hunger").append(myTamagotchi.hunger);
-$("#sleepiness").append(myTamagotchi.sleepiness);
-$("#boredom").append(myTamagotchi.boredom);
-$("#age").append(myTamagotchi.age);
+// create tamagotchi object
+let myTamagotchi = new Tamagotchi(1, 1, 1, 0);
+
+//create functions to increment stats
+
+function incrementHunger() {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.hunger += 1;
+        $("#hunger").replaceWith(`<p id='hunger'>Hunger: ${myTamagotchi.hunger}</p>`);
+        incrementHunger();
+    }, 15000)
+
+};
+
+function incrementSleepiness() {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.sleepiness += 1;
+        $("#sleepiness").replaceWith(`<p id='sleepiness'>Sleepiness: ${myTamagotchi.sleepiness}</p>`);
+        incrementSleepiness();
+    }, 20000)
+
+};
+
+function incrementBoredom() {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.boredom += 2;
+        $("#boredom").replaceWith(`<p id='boredom'>Boredom: ${myTamagotchi.boredom}</p>`);
+        incrementBoredom();
+    }, 20000)
+
+};
+
+function hatch() {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.age += 1;
+        $("#age").replaceWith(`<p id='age'>Age: ${myTamagotchi.age}</p>`);
+        birth();
+    }, 10000)
+}
+
+function incrementAge() {
+    let timeoutID = window.setTimeout(function() {
+        myTamagotchi.age += 1;
+        $("#age").replaceWith(`<p id='age'>Age: ${myTamagotchi.age}</p>`);
+        incrementAge();
+    }, 5000)
+
+}
+
+function birth() {
+    if (myTamagotchi.age < 1) {
+        hatch();
+    } else {
+        $("img:eq(1)").replaceWith("<img src='tamagotchi-bunny-2.png'>")
+        incrementAge();
+    }
+}
+
+incrementHunger();
+incrementSleepiness();
+incrementBoredom();
+birth();
